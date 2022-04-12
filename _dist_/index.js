c0496b2971ec1f863b76abf469710c066182c9fd
Grigerlord <Grigerlord@users.noreply.github.com>
2022-04-12 23:05:00 +0000

    deploy: d74fbc6179147b44cc2f0bdd7ed20604f73878ea

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const numRandom = () => {
+    const random = Math.floor(Math.random() * (122 - 1) + 1)
+    return random
+}
 
-console.log('Happy hacking :)')
+
+const createImageNode = () => {
+    const container = document.createElement('div')
+    container.className = 'p-4'
+
+    const imagen = document.createElement('img')
+    imagen.className = 'mx-auto'
+    imagen.width = '320'
+    imagen.src = `https://randomfox.ca/images/${numRandom()}.jpg` //TODO
+
+    container.appendChild(imagen)
+
+    return container
+}
+
+const nuevaImagen = createImageNode()
+const mountNode = document.getElementById('images')
+
+const addButton = document.querySelector('button')
+
+//
+const addImage = () => {
+    const newImage = createImageNode()
+    mountNode.append(newImage)
+}
+
+addButton.addEventListener('click', addImage)
